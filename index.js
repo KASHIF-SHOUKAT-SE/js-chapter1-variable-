b0ed4addb14ec9a803a3191659c2dbe15c4d205f
KASHIF-SHOUKAT-SE <149269224+KASHIF-SHOUKAT-SE@users.noreply.github.com>
2024-04-23 04:16:38 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+// nn bb ss u primetive data type
+let a = 325;
+let b = null;
+let c = true;
+let d = BigInt("345");
+let e = "kashif";
+let f = Symbol(" i am a student");
+let g = undefined;
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+console.log(e)
+console.log(f)
+console.log(g)
+console.log(typeof a)
+
+// object in js non primetive data type
+const item = {
+    "kashif": true,
+    "waleed": false,
+    "naeem": 764,
+    "majid": undefined,
+}
+console.log(item["kashif"])
